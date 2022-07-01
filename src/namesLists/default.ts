d4feacf0923152e6c2b0e7e98ed9899a54f9149b
Andrey Andrianov <trancemilly7@gmail.com>
2022-07-01 13:10:52 +0300

    Create default.ts

---
+++ src/namesLists/default.ts
@@ -0,0 +1,22 @@
+export class namesList{ 
+    namesList = [
+        "name1"
+    ];
+    
+    emailsList = [
+        "email1"
+    ];
+    
+    walletsList = [ 
+        "wallet1"
+    ];
+    
+    telegramsList = [
+        "@telegram1"
+    ];
+    
+    antyUsername = 'dolphyn_email';
+    antyPassword = 'dolphyn_pass';
+
+}
+export default namesList;
